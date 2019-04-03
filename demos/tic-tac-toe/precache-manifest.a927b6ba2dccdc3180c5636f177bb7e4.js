9e157fb966a4a76bbdc5103cc6ade28eb4d38cf1
Natalie Reinicke <nataliereinicke@natalies-air.lan>
2019-04-02 21:23:41 -0700

    Tic Tac Toe demo

---
+++ demos/tic-tac-toe/precache-manifest.a927b6ba2dccdc3180c5636f177bb7e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38f9185d3f068febacb1",
+    "url": "/static/css/main.634f0ccc.chunk.css"
+  },
+  {
+    "revision": "38f9185d3f068febacb1",
+    "url": "/static/js/main.38f9185d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f0bfb5bd22f5182869e7",
+    "url": "/static/js/2.f0bfb5bd.chunk.js"
+  },
+  {
+    "revision": "f402817244e7af1f462e3b51240a0667",
+    "url": "/index.html"
+  }
+];
