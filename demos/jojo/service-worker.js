42f7e7822c11cd5637afa9e91d89619e8c39dd62
Natalie Reinicke <nataliereinicke@natalies-air.lan>
2019-04-08 20:45:38 -0700

    added jojo demo

--- demos/jojo/service-worker.js
+++ demos/jojo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e55ff0eae6c83be48bc7c393dc61c9a0.js"
+  "/demos/jojo/precache-manifest.e4d2a39102cb88cf1ae905bae2c12f96.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/demos/jojo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
